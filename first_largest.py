807eba7b77d8ea25c771e54fd06270791a0bcdc2
Zak <zcoster@ksu.edu>
2021-09-24 12:17:16 -0500

    Adding try except

--- first_largest.py
+++ first_largest.py
@@ -5,16 +5,26 @@ Python program to find the largest element and its location.
 def largest_element(a):
     """ Return the largest element of a sequence a.
     """
-    maxval = a[0]
-    loc = 0
-    for i in range(1, len(a)):
-        if a[i] > maxval:
-            maxval = a[i]
-            loc = i
-    return maxval, loc
+    try:
+        maxval = a[0]
+        loc = 0
+        for i, f in enumerate(a):
+            if a[i] > maxval:
+                maxval = a[i]
+                loc = i
+        return maxval, loc
+    except ValueError:
+        print('ValueError Nerd')
+        return -1
+    except TypeError:
+        print('TypeError nerd')
+        return -2
+    except Exception as e:
+        print(f'A more different error nerd: {e}')
+        return -3
 
 
 if __name__ == "__main__":
 
-    a = [1,2,3,2,1]
+    a = [1,'a', 2]
     print("Largest element is {:}".format(largest_element(a)))
